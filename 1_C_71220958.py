e351a9386bfe93f175332f6044c7b085455b34e6
KevinGloryPrasetyo-71220958 <117893068+KevinGloryPrasetyo-71220958@users.noreply.github.com>
2022-11-24 16:34:13 +0700

    Add files via upload

---
+++ 1_C_71220958.py
@@ -0,0 +1,44 @@
+print("============PILIH MENU========")
+print("1.Tambah")
+print("2.Kurang")
+print("3.Kali")
+print("4.Bagi")
+billpertama = int(input("Masukan Angka Pertama:"))
+billkedua = int(input("Masukan Angka Kedua:"))
+print ("pilihan anda:1")
+print("hasil", billpertama+billkedua)
+
+print("============PILIH MENU========")
+print("1.Tambah")
+print("2.Kurang")
+print("3.Kali")
+print("4.Bagi")
+billpertama = int(input("Masukan Angka Pertama:"))
+billkedua = int(input("Masukan Angka Kedua:"))
+print ("pilihan anda:2")
+print("hasil", billpertama-billkedua)
+
+print("============PILIH MENU========")
+print("1.Tambah")
+print("2.Kurang")
+print("3.Kali")
+print("4.Bagi")
+billpertama = int(input("Masukan Angka Pertama:"))
+billkedua = int(input("Masukan Angka Kedua:"))
+print ("pilihan anda:3")
+print("hasil", billpertama*billkedua)
+
+print("============PILIH MENU========")
+print("1.Tambah")
+print("2.Kurang")
+print("3.Kali")
+print("4.Bagi")
+billpertama = int(input("Masukan Angka Pertama:"))
+billkedua = int(input("Masukan Angka Kedua:"))
+print ("pilihan anda:4")
+print("hasil", billpertama/billkedua)
+
+
+
+
+
---
+++ 3_C_71220958.py
@@ -0,0 +1,13 @@
+print("Masukan Daftar Pesanan : eskrim, es teh, putih")
+print("Daftar Pesanan : ['Eskrim','Es Teh','Putih']")
+Daftar = int(input("Masukan Pesanan yang ingin ditambahkan :"))
+print ("sudah berada di dalam pesanan:")
+
+daftar = input("Masukan Daftar Pesanan:")
+pedagang = daftar.split()
+print("Daftar Pesanan:", pedagang)
+nambah = input("Masukan Pesanan Yang Ingin Ditambhkan")
+pedagang.append(nambah)
+print("Hasil Penambahan dalam pesanan:", pedagang)
+    
+

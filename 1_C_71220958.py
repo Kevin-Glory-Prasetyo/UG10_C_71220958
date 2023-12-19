79d93e43fccd11237d32a31e7fd5a8ecaed8f72a
KevinGloryPrasetyo-71220958 <117893068+KevinGloryPrasetyo-71220958@users.noreply.github.com>
2023-12-19 14:03:06 +0700

    Update 1_C_71220958.py

--- 1_C_71220958.py
+++ 1_C_71220958.py
@@ -41,3 +41,38 @@ print("hasil", billpertama/billkedua)
 
 
 
+while True:
+    print("\nPilih Menu:")
+    print("1. Tambah Penduduk\n2. Tampilkan Urut Nama\n3. Tampilkan Urut Usia")
+    
+    try:
+        pilihan = int(input("Pilihan Anda: "))
+    except ValueError:
+        print("Masukkan angka sebagai pilihan.")
+        continue
+
+    if pilihan == 1:
+        nama = input("Masukkan Nama: ")
+        try:
+            usia = int(input("Masukkan Usia: "))
+        except ValueError:
+            print("Masukkan angka sebagai usia.")
+            continue
+
+        root = tambah_penduduk(root, nama, usia)
+        print("Data berhasil ditambahkan!")
+
+    elif pilihan == 2:
+        print("Daftar Penduduk:")
+        tampilkan_urut_nama(root)
+
+    elif pilihan == 3:
+        print("Daftar Penduduk:")
+        tampilkan_urut_usia(root)
+
+    else:
+        print("Pilihan tidak valid. Silakan pilih kembali.")
+
+
+
+
